47b17457d879cc91806539fd35eef09442d97902
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2019-01-22 11:39:16 +0000

    working ocsp check

--- prometheus_client.sh
+++ prometheus_client.sh
@@ -18,4 +18,9 @@ bundle check || bundle install
 
 docker run --rm --name prometheus -d -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml -p 9090:9090 prom/prometheus
 
-bundle exec bin/prometheus-metadata-exporter -h https://${signin_domain}/SAML2/metadata/federation
+CAS=../verify-puppet/modules/ida_truststore/files/federation_ca_certs/idap-core-test-ca.crt,../verify-puppet/modules/ida_truststore/files/federation_ca_certs/idap-relying-party-test-ca.crt,../verify-puppet/modules/ida_truststore/files/federation_ca_certs/idap-test-ca.crt,../verify-puppet/modules/ida_truststore/files/federation_ca_certs/idap-metadata-test-ca.crt,../verify-puppet/modules/ida_truststore/files/federation_ca_certs/idap-test-root-ca.crt
+if [ "$env" == "" ]; then
+    CAS=../verify-puppet/modules/ida_truststore/files/federation_ca_certs/idap-core-ca-g2.crt,../verify-puppet/modules/ida_truststore/files/federation_ca_certs/idap-relying-party-ca-g2.crt,../verify-puppet/modules/ida_truststore/files/federation_ca_certs/idap-ca-g2.crt,../verify-puppet/modules/ida_truststore/files/federation_ca_certs/idap-metadata-ca-g2.crt,../verify-puppet/modules/ida_truststore/files/federation_ca_certs/idap-root-ca.crt
+fi
+
+bundle exec bin/prometheus-metadata-exporter -h https://${signin_domain}/SAML2/metadata/federation --cas $CAS
